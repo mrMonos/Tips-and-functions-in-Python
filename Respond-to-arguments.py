fd5b0ae8713e277569303d2f7ade32241bd983cf
mrMonos <131591271+mrMonos@users.noreply.github.com>
2023-04-23 15:47:28 +0200

    Update and rename test.py to Respond-to-arguments.py

---
+++ Respond-to-arguments.py
@@ -0,0 +1,9 @@
+import sys
+
+arg = sys.argv
+
+
+# In the empty "" write the arguments you are looking for 
+
+if "" in arg:
+    # Here write the command you want to call if you find the arguments you are looking for
--- test.py
+++
@@ -1,8 +0,0 @@
-import sys
-import re
-
-n = sys.argv
-
-
-if "new" in n:
-    print("New")
